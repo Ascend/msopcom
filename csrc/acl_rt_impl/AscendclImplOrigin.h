a7b4b586d2481b1a1eb93b14fc864da1132e0b11
wiyr0 <wangyixian3@huawei.com>
2026-07-22 19:25:35 +0800

    [bugfix][sync] 设备id转换适配 aclrtGetPhyDevIdByLogicDevId 接口
    Co-authored-by: wiyr0<wangyixian3@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !195 merge fix_bug_26_1_0 into 26.1.0
    
    [bugfix][sync] 设备id转换适配 aclrtGetPhyDevIdByLogicDevId 接口
    
    Created-by: wiyr0
    Commit-by: wiyr0
    Merged-by: ascend-robot
    Description: ### 1. 修改描述
    - **修改原因：**  
    - **修改方案：**
    - **修改内容：**
    - [x] **涉及代码双合**(贴上另一个PR链接)：https://gitcode.com/Ascend/msopcom/pull/193
    ----
    
    ### 2. 功能验证
    - [x] **功能自验截图**（请确保不体现个人信息）  
    - [x] **冒烟是否通过**  
    ----
    
    ### 3. 代码检视
    - **要求：**
      - 合入功能代码大于 200 行，需要sig会议申报代码检视议题，并在PR中标注会议。
      - committer评估是否需要在sig会议进行代码检视。
      - 参与检视的committer人员名单与检视时间。
      - 大于 1000 行代码原则上不允许合入，需进行备案。
    - [x] **是否经过代码检视**  
    - [ ] **是否具备UT测试用例看护**
    - [ ] **是否需要在sig会议中进行代码检视**
    - **检视committer人员名单与检视时间：**
    ----
    ### 4. 资料修改自检
    - **资料修改：**    
    ----
    
    See merge request: Ascend/msopcom!195

--- csrc/acl_rt_impl/AscendclImplOrigin.h
+++ csrc/acl_rt_impl/AscendclImplOrigin.h
@@ -25,7 +25,7 @@ void AscendclImplOriginCtor();
 aclError aclrtSetDeviceImplOrigin(int32_t deviceId);
 aclError aclrtGetDeviceImplOrigin(int32_t *deviceId);
 aclError aclrtResetDeviceForceImplOrigin(int32_t deviceId);
-aclError aclrtGetUserDevIdByLogicDevIdImplOrigin(const int32_t logicDevId, int32_t *const userDevid);
+aclError aclrtGetPhyDevIdByLogicDevIdImplOrigin(const int32_t logicDevId, int32_t *const phyDevId);
 const char *aclrtGetSocNameImplOrigin();
 
 aclError aclrtMallocImplOrigin(void **devPtr, size_t size, aclrtMemMallocPolicy policy);
